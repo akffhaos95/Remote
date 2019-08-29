8e16dba9d48eb4c1ba1e29cf5df733237341df28
akffhaos95_LAPT <a01044234369@gmail.com>
2019-08-29 16:20:21 +0900

    Add Layout

--- app/src/main/java/com/example/remote/MainActivity.java
+++ app/src/main/java/com/example/remote/MainActivity.java
@@ -61,14 +61,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View view) {
         switch(view.getId()){
-            case R.id.connBtn:
-                ip = textIP.getText().toString();
-                port = Integer.parseInt(textPort.getText().toString());
-                break;
             case R.id.voiceBtn:
                 ip = textIP.getText().toString();
                 port = Integer.parseInt(textPort.getText().toString());
                 speak();
+            case R.id.listBtn:
+
         }
     }
 
