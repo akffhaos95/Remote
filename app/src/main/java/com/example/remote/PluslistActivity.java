cd148a859fe4090b414b868ebab3c914238b8344
akffhaos95_7508 <akffhaos95@naver.com>
2019-09-18 09:45:19 +0900

    PlusActivity 추가 방법 변경

--- app/src/main/java/com/example/remote/PluslistActivity.java
+++ app/src/main/java/com/example/remote/PluslistActivity.java
@@ -3,23 +3,26 @@ package com.example.remote;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.Locale;
 
 public class PluslistActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final int REQUEST_CODE_SPEECH_INPUT = 1000;
     public final String PREFERENCE = "com.example.remote";
-    TextView titleView;
-    EditText nameText, modelText;
-    Button applyBtn;
-    String title, model, name;
+    TextView titleView, nameText, modelText;
+    Button applyBtn, nameBtn, modelBtn;
+    String title, model, name, name_ck, model_ck, speakname;
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -37,9 +40,13 @@ public class PluslistActivity extends AppCompatActivity implements View.OnClickL
         nameText = findViewById(R.id.nameText);
         modelText = findViewById(R.id.modelText);
         applyBtn = findViewById(R.id.applyBtn);
+        nameBtn = findViewById(R.id.nameBtn);
+        modelBtn = findViewById(R.id.modelBtn);
 
         titleView.setText(title);
         applyBtn.setOnClickListener(this);
+        nameBtn.setOnClickListener(this);
+        modelBtn.setOnClickListener(this);
     }
     @Override
     public void onClick(View view) {
@@ -50,18 +57,69 @@ public class PluslistActivity extends AppCompatActivity implements View.OnClickL
                 if(name.length()==0 || model.length()==0){
                     Toast.makeText(this, "빈칸이 있습니다.", Toast.LENGTH_SHORT).show();
                 } else {
-                    //호출명(name)은 나중에 음성 입력으로 처리
                     save();
                     finish();
                 }
+                break;
+            case R.id.nameBtn:
+                speak();
+                break;
+            case R.id.modelBtn:
+                //DB 액티비티 만들기
                 break;
         }
     }
     protected void save(){
         SharedPreferences pref = getSharedPreferences(PREFERENCE, MODE_PRIVATE);
         SharedPreferences.Editor editor = pref.edit();
+        Collection<?> name_check =  pref.getAll().keySet();
+        Collection<?> model_check =  pref.getAll().keySet();
+        Iterator<?> it = name_check.iterator();
+        Iterator<?> it2 = model_check.iterator();
+        while(it.hasNext()) {
+            name_ck = (String)it.next();
+            if(name == name_ck){
+                Toast.makeText(this, "이름이 중복됩니다.", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+        while(it2.hasNext()){
+            if(model == model_ck) {
+                model_ck = (String) it.next();
+                Toast.makeText(this, "모델명이 중복됩니다.", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
         editor.putString(name,model);
         editor.commit();
         Toast.makeText(this,"저장 완료",Toast.LENGTH_SHORT).show();
+    }
+
+    private void speak() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "호출명 말해주세요");
+        try {
+            startActivityForResult(intent, REQUEST_CODE_SPEECH_INPUT);
+        }
+        catch (Exception e){
+            Toast.makeText(this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch(requestCode){
+            case REQUEST_CODE_SPEECH_INPUT: {
+                if(resultCode == RESULT_OK && null !=data){
+                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    speakname = result.get(0);
+                    nameText.setText(speakname);
+                }
+                break;
+            }
+        }
     }
 }
